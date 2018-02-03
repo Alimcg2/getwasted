647f07c772c73d7f24cd575e234168ccc35f0b0c
Alison McGuire <=>
2018-02-02 18:43:13 -0800

    sos it almost works but doesn't

--- webtest.py
+++ webtest.py
@@ -1,6 +1,8 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
+nonWords = "with, at, from, into, during, including, until, against, among, throughout, the, how, my, or, to, and, a, no, towards, upon, concerning, of, to, in, for, on, by, about, like, through, over, before, between, after, since, without, under, within, along, following, across, behind, beyond, plus, except, but, up, around, down, off, above, near, day".split(", ")
 
 def getPostUrls(pages):
     postUrls = []
@@ -18,16 +20,56 @@ def getPostUrls(pages):
 
 
 def getPostContent(postUrls):
-    content = {}
+    content = []
     for post in postUrls:
         req = requests.get(post)
         soup = BeautifulSoup(req.content, 'html.parser')
-        # get title / CHANGE THIS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        if len(soup.find_all(class_="entry-title")) > 0:
-            print soup.find_all(class_="entry-title")[0].get_text()
-            
-            
         
+        # get title / CHANGE THIS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        getTitle = soup.find_all(class_="entry-title")
+        if len(soup.find_all(class_="entry-title")) > 0:
+            content.append(soup.find_all(class_="entry-title")[0].get_text())
+            title = soup.find_all(class_="entry-title")[0].get_text()
+        else:
+            content.append("NULL - title")
+            title = "NULL - keywords"
+
+        # get image / CHANGE THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        getImage = soup.find_all(class_="thumb-image")
+        if soup.find_all(class_="thumb-image"):
+            content.append(getImage[0]['data-image'])
+        else:
+            content.append("NULL - image")
+
+        # get date / CHANGE THIS!!!!!!!!!!!!!!!!!!!!!!!!!11!!!!!
+        getDate = soup.find_all(class_="dt-published")
+        if len(getDate) > 0:
+            content.append(getDate[0].get_text())
+        else:
+            content.append("NULL - date")
+
+        # writes post URL
+        content.append(post)
+    
+        # write Blog Name / CHANGE THIS!!!!!!!!!!!!!!!!!!!!!!!
+        content.append("Going Zero Waste")
+
+        # getkeywords
+        words = title.split(" ")
+        keywords = ""
+        for word in words:
+            if word.lower() not in nonWords && !is_number(word):
+                keywords = keywords + " " + word
+        content.append(keywords.strip())
+        writeContent(content)
+                
+
+def writeContent(content):
+    with open('output', 'a') as f:
+        for item in content:
+            print(item)
+            f.write(item + '\t')
+        f.write('\n')
 
 def main():
     
